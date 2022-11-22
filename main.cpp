069a7c60830440b077a42187dfc6022e933c552e
Sink <468766131@qq.com>
2022-11-22 18:48:25 +0800

    add ffmpeg library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "main_window.h"
-
+#include <QDebug>
 #include <QApplication>
+
+extern "C"{
+    #include <libavutil/avutil.h>
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    qDebug() << av_version_info() << Qt::endl;
     return a.exec();
 }
